3a719c754e041f048eee7af14128f1a080b9c77b
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-29 20:10:25 -0500

    Preview image to upload

--- img-drive/client/client.js
+++ img-drive/client/client.js
@@ -1,1 +1,24 @@
-console.log('test')
+function updateImageDisplay () {
+  // If there is something inside preview, remove it.
+  while (preview.firstChild) {
+    preview.removeChild(preview.firstChild);
+  }
+
+  // Get current files.
+  const curFiles = input.files;
+
+  // If no files selected, show paragraph, otherwise, show image preview
+  if (!curFiles.length) {
+    const para = document.createElement('p');
+    para.textContent = 'No files currently selected for upload';
+    preview.appendChild(para);
+  } else {
+    const image = document.createElement('img');
+    image.src = window.URL.createObjectURL(curFiles[0]);
+    preview.appendChild(image);
+  }
+}
+
+const preview = document.querySelector('.preview');
+const input = document.querySelector('input');
+input.addEventListener('change', updateImageDisplay);
