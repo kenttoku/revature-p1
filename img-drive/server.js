6ce04f9a089fe9c350e55f5469f827e787fd2d8b
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-29 22:28:56 -0500

    Send photo back to user

--- img-drive/server.js
+++ img-drive/server.js
@@ -9,10 +9,16 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static('./client'));
 
+// upload file. currently uploading to a directory
+// replace with uploading to a DB later
 app.post('/upload', upload.single('photo'), (req, res) => {
   if (req.file) {
     res.json(req.file);
   }
+});
+
+app.get('/pictures', (req, res) => {
+  res.sendFile('/Users/kent/revature-p1/img-drive/uploads/images/093256e7e9dd532222ece1139a24d006');
 });
 
 // eslint-disable-next-line no-console
