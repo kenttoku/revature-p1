d2fd942eccd74b5c4569c42c0c200476941c6394
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-03 17:53:44 -0500

    Allow container access on image container

--- create-web-app.sh
+++ create-web-app.sh
@@ -18,9 +18,6 @@ blobStorageAccountKey=$(az storage account keys list -g $resource_group \
   -n $blobStorageAccount --query [0].value --output tsv)
 
 az storage container create -n images --account-name $blobStorageAccount \
---account-key $blobStorageAccountKey --public-access off
-
-az storage container create -n thumbnails --account-name $blobStorageAccount \
 --account-key $blobStorageAccountKey --public-access container
 
 echo "Make a note of your Blob storage account key..."
