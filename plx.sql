ecb89d28eef3fa3a14846648850ecb68a0ef5a26
Kent Tokunaga <KentTokunaga@gmail.com>
2019-05-06 10:20:11 -0500

    Create table for images

--- plx.sql
+++ plx.sql
@@ -1,7 +1,14 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS images;
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
   username text NOT NULL UNIQUE,
   password text NOT NULL UNIQUE
+);
+
+CREATE TABLE images (
+  id serial PRIMARY KEY,
+  url text NOT NULL UNIQUE,
+  user text NOT NULL
 );
